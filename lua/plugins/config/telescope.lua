d13a308f66c897d5aed68ff742d7c80c35c19251
angelollima <angeloantonio442@gmail.com>
2023-01-18 09:47:45 -0300

    Mapping <C-a> for vsplit, <C-s> for split

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,5 +1,17 @@
+local actions = require("telescope.actions")
+
 require("telescope").setup({
 	defaults = {
+    mappings = {
+      i = {
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-a>"] = actions.select_vertical,
+      },
+      n = {
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-a>"] = actions.select_vertical,
+      },
+    },
 		prompt_prefix = "● ",
 		selection_caret = "  ",
 		entry_prefix = "  ",
