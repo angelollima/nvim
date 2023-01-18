89e110f5b7e7fdde79f4bae047c71f4085aad80a
angelollima <angeloantonio442@gmail.com>
2023-01-18 09:54:39 -0300

    Adding Rule for react fragment

--- lua/plugins/config/autopairs.lua
+++ lua/plugins/config/autopairs.lua
@@ -1,1 +1,7 @@
+local Rule = require('nvim-autopairs.rule')
 require("nvim-autopairs").setup({})
+
+require('nvim-autopairs').add_rules {
+    Rule("%<%>$", "</>", { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' })
+            :use_regex(true),
+}
