152c72295f573af4e64c31872622ff784312301e
angelollima <angeloantonio442@gmail.com>
2023-01-18 09:44:53 -0300

    adding glow.nvim, pantran.nvim, mini.move

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -12,7 +12,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim") -- packer manage itself
 
 	use("lewis6991/impatient.nvim")
 
@@ -20,14 +20,23 @@ return require("packer").startup(function(use)
 
 	use("sainnhe/gruvbox-material")
 
-	use("mrshmllow/document-color.nvim")
+  use("norcalli/nvim-colorizer.lua") -- Add color
 
-	use("windwp/nvim-autopairs")
+	use("mrshmllow/document-color.nvim") -- Add color
+
+	use("windwp/nvim-autopairs") -- Autocomplete (), {}, []
+
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+  })
+
+  use('echasnovski/mini.move') -- Move lines around
 
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		requires = {
-			"windwp/nvim-ts-autotag",
+			"windwp/nvim-ts-autotag", -- Complete tags, functions and so fourth...
 		},
 		run = ":TSUpdate",
 	})
@@ -37,10 +46,10 @@ return require("packer").startup(function(use)
 		requires = {
 			"hrsh7th/nvim-cmp",
 			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-nvim-lsp-signature-help", -- kinda help to functions
+			"L3MON4D3/LuaSnip", -- snippet engine
+			"saadparwaiz1/cmp_luasnip", -- snippet completions
+			"rafamadriz/friendly-snippets", -- snippets for a lot of different languages
 			"williamboman/nvim-lsp-installer",
 			"aca/emmet-ls",
 			"j-hui/fidget.nvim",
@@ -50,14 +59,14 @@ return require("packer").startup(function(use)
 
 	use({ "mfussenegger/nvim-dap", requires = { "rcarriga/nvim-dap-ui" } })
 
-	use("mhartington/formatter.nvim")
+	use("mhartington/formatter.nvim") -- Formate code
 
 	use("numToStr/Comment.nvim")
 
 	use("lewis6991/gitsigns.nvim")
 
 	use({
-		"nvim-telescope/telescope.nvim",
+		"nvim-telescope/telescope.nvim", -- Find files
 		requires = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-fzf-native.nvim",
@@ -70,6 +79,10 @@ return require("packer").startup(function(use)
 		"kyazdani42/nvim-tree.lua",
 		requires = "kyazdani42/nvim-web-devicons",
 	})
+
+  use("potamides/pantran.nvim") -- Window Translate
+
+  use("ellisonleao/glow.nvim") -- Preview markdown
 
 	if packer_bootstrap then
 		require("packer").sync()
